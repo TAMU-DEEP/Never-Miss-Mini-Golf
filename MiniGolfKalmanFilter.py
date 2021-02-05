c041cbbc401a267054e1a4090434813feafb7563
rymuelle <rmueller@physics.tamu.edu>
2021-02-05 15:32:48 -0600

    First version of MiniGolfKalmanFilter.py

---
+++ MiniGolfKalmanFilter.py
@@ -0,0 +1,75 @@
+import numpy as np
+from filterpy.kalman import KalmanFilter
+from filterpy.common import Q_discrete_white_noise
+
+#in this class, we inherit from the KalmanFilter class and make a specific version for our use case
+class MiniGolfKalmanFilter(KalmanFilter):
+    def __init__(self,
+    intial_state=np.array([0.,0.,0.,0.]),
+    fc = 1.,
+    dt = 1.,
+    R_val = 5,
+    Q_val = 0.13,
+    ):
+        #the super funciton runs the constructor of the KalmanFilter class and inherits it's methods for our class 
+        super().__init__(dim_x=4, dim_z=2)
+        #friction constant
+        self.intial_state  = intial_state
+        self.fc            = fc
+        self.dt            = 1
+        self.P            *= 1000.
+        self.R            *= R_val
+        self.Q             = Q_discrete_white_noise(dim=4, dt=dt, var=Q_val)
+        self.x             = intial_state
+        self.F             = np.array([
+                                      [1,    0,  dt,   0],
+                                      [0,    1,  0,    dt],
+                                      [0,    0,  fc,   0],
+                                      [0,    0,  0,    fc]
+                                      ])
+        self.H             = np.array([
+                                      [1.,    0., 0.,  0.], 
+                                      [0.,    1., 0.,  0.],
+                                      ])
+
+    def print_state(self):
+        print("\tcurrent possition: ({},{}) velocity: ({},{})".format(*self.x))
+
+if __name__=="__main__":    
+    print("testing kalman filter wrapper")
+    #define some constants
+    x,y,dx,dy = 0,0,.5,1
+    intial_state = np.asarray([x,y,dx,dy])
+
+    ##
+    ## Inital state:
+    ##
+
+    #create our object
+    filter_eg = MiniGolfKalmanFilter(intial_state=intial_state)
+    filter_eg.print_state()
+
+    #initial_state check
+    assert (filter_eg.x==intial_state).all(), "inital state wrongly initialized"
+
+    ##
+    ## After one step:
+    ##
+
+    #update once
+    filter_eg.predict()
+    print("after one step:")
+    filter_eg.print_state()
+
+    #predict check
+    assert (filter_eg.x==np.asarray([.5,1,.5,1])).all(), "After one step, needs to be one distance down the line"
+
+    ##
+    ## After update:
+    ##
+
+    #update once
+    filter_eg.update([-10,-10])
+    filter_eg.predict()
+    print("after updating with -10,-10")
+    filter_eg.print_state()
